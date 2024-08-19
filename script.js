edb96942fca2d4208f9082bf7db174bff1f21075
Aditya Kumar <114895058+adityakqumar@users.noreply.github.com>
2024-08-19 22:23:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript to handle dynamic elements
+
+function contactMe() {
+    alert("Thanks for your interest! Please reach out to me via LinkedIn or Behance.");
+}
+
+document.getElementById('viewAllBtn').addEventListener('click', function() {
+    alert('All projects will be shown here.');
+});
+
+// You can add more JS to handle dynamic content as needed
